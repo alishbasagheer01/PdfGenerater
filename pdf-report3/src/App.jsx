fe225df344bdce3da1f61835a9eaef33d5a229c4
alishbasagheer01 <alishbasagheer01@gmail.com>
2025-08-15 06:29:18 -0700

    added another report

--- pdf-report3/src/App.jsx
+++ pdf-report3/src/App.jsx
@@ -27,6 +27,22 @@ export default function App() {
     <div style={{ padding: 20 }}>
       <h1>Download Report</h1>
       <PDFDownloadLink
+        document={<ChatsReport2/>}
+        fileName="daily_report.pdf"
+        style={{
+          padding: "10px 20px",
+          backgroundColor: "#4CAF50", 
+          color: "#fff",
+          textDecoration: "none",
+          borderRadius: "5px",
+        }}
+      >
+        {({ loading }) => (loading ? "Loading..." : "Download PDF")}
+      </PDFDownloadLink>
+    </div>
+    <div style={{ padding: 20 }}>
+      <h1>Download Report</h1>
+      <PDFDownloadLink
         document={<SocialMedia/>}
         fileName="daily_report.pdf"
         style={{
